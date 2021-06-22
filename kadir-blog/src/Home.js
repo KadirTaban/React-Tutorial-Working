0c2144b568682677e2b2b6f96f1fd701bb35c007
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-06-22 13:12:41 +0300

    Outputting Lists

--- kadir-blog/src/Home.js
+++ kadir-blog/src/Home.js
@@ -1,22 +1,22 @@
-const Home = () => {
+import { useState } from 'react';
 
-    const handleClick = () => {
-        console.log('hello,ninjas');
-    }
-    const handleClickAgain = (name,e) =>{
-        console.log("hello"+name,e);
-    }
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website',body:'lorem ipsum...',author: ' mario',id: 1},
+        { title: 'Welcome party!',body:'lorem ipsum...',author: 'Kadir',id: 2},
+        { title: 'Web dev top tips', body:'lorem ipsum...',author:'sare',id: 3}
+
+    ]);
+    
     return(
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => {
-                handleClickAgain('mario',e)
-            }}>Click me again</button>
-
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
     );
-
-
 }
 export default Home;
