8b3da94170b8041940a36753d91bd1be0e8affa1
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-06-23 12:45:58 +0300

    Functions

--- kadir-blog/src/App.js
+++ kadir-blog/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+
 function App() {
   return (
     <div className="App">
--- kadir-blog/src/Home.js
+++ kadir-blog/src/Home.js
@@ -1,21 +1,20 @@
 import { useState } from 'react';
-
+import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website',body:'lorem ipsum...',author: ' mario',id: 1},
-        { title: 'Welcome party!',body:'lorem ipsum...',author: 'Kadir',id: 2},
+        { title: 'Welcome party!',body:'lorem ipsum...',author: 'kadir',id: 2},
         { title: 'Web dev top tips', body:'lorem ipsum...',author:'sare',id: 3}
-
     ]);
     
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog=> blog.id !== id);
+        setBlogs(newBlogs);
+    }
     return(
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete= {handleDelete}/>
+             
         </div>
     );
 }
