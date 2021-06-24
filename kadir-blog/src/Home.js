ba2be86b16fb5f5fc7cadd3e7a62accb11a68fdb
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-06-24 12:08:24 +0300

    making a custom hook

--- kadir-blog/src/Home.js
+++ kadir-blog/src/Home.js
@@ -1,46 +1,13 @@
 import { useState , useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending,setIsPending] = useState(true);
-    const [error,setError] = useState(null);
-
-
-    
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog=> blog.id !== id);
-    }
-
-    useEffect(() => {
-        setTimeout(()=> {
-            fetch('http://localhost:8000/blogss')
-            .then(res => {
-                console.log(res);
-                if (!res.ok){
-                    throw Error('could not fetch the data for that resource');
-
-                }
-
-                return res.json();
-            })
-            .then((data) =>{
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setIsPending(false)
-                setError(err.message);
-            })
-        
-
-        },1000)
-    }, []);
+  const { data: blogs ,isPending, error } = useFetch("http://localhost:8000/blogs");
     return(
         <div className="home">
         { error && <div> { error } </div> }
         { isPending && <div>Loading....</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete= {handleDelete} />  } 
+        {blogs && <BlogList blogs={blogs} title="All Blogs!" />  } 
             
         </div>
     );
