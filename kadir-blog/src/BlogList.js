3fcd5654c8be4750b9b75b3a2f0c18f54beaaecd
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-06-27 16:55:13 +0300

    add bkıg

--- kadir-blog/src/BlogList.js
+++ kadir-blog/src/BlogList.js
@@ -4,8 +4,11 @@ const BlogList = ({blogs,title,handleDelete}) => {
             <h2>{ title }</h2> 
             {blogs.map((blog) => (
                 <div className="blog-preview" key = {blog.id}>
+                   <Link to ={'/blogs/${blog.id}'}>
                     <h2>{ blog.title }</h2>
                     <p>Written By { blog.author }</p>
+                   </Link>
+
                 </div>
             ))}
         </div>
